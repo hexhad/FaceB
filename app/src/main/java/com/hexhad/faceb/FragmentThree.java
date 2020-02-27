19cc1433ad44e548424e505aafdc15bf665db671
Sandun <49017841+Sandunrmst@users.noreply.github.com>
2020-02-28 01:19:58 +0530

    Search update

--- app/src/main/java/com/hexhad/faceb/FragmentThree.java
+++ app/src/main/java/com/hexhad/faceb/FragmentThree.java
@@ -2,12 +2,20 @@ package com.hexhad.faceb;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
 
 public class FragmentThree extends Fragment {
 
@@ -20,6 +28,11 @@ public class FragmentThree extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragmenttree,container,false);
+        return inflater.inflate(R.layout.fragmentthree,container,false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
     }
 }
